c2864643c3eb048511adf7f499e41cf45d4cbefd
Karl Belousov <belousovkarl@gmail.com>
2024-06-06 17:14:43 +0600

    исправил отображение рейтинга фильма

--- src/view/film-card-info-template.js
+++ src/view/film-card-info-template.js
@@ -10,7 +10,7 @@ export const createFilmsCardInfoTemplate = (filmInfo, commentsLength) => {
   return `
     <a class="film-card__link">
       <h3 class="film-card__title">${title}</h3>
-      <p class="film-card__rating"${totalRating}</p>
+      <p class="film-card__rating">${totalRating}</p>
       <p class="film-card__info">
         <span class="film-card__year">${formatStringToYear(release.date)}</span>
         <span class="film-card__duration">${formatMinutesToTime(runtime)}</span>
